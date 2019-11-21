ad395396d15d1990437ae6ca2329ef050a4b085f
lvfei418 <56762392+lvfei418@users.noreply.github.com>
2019-11-21 17:58:23 +0800

    Create Complex.cpp

---
+++ 201816040226/Lab4/Complex.cpp
@@ -0,0 +1,44 @@
+// Lab 4: Complex.cpp
+// Member-function definitions for class Complex.
+#include <iostream>
+using namespace std;
+
+#include "Complex.h"
+
+Complex::Complex( double real, double imaginary )
+{
+   setComplexNumber( real, imaginary );
+} // end Complex constructor
+
+Complex Complex::add( const Complex &right )
+{
+   /* Write a statement to return a Complex object. Add
+      the realPart of right to the realPart of this Complex
+      object and add the imaginaryPart of right to the
+      imaginaryPart of this Complex object */
+      realPart=realPart+right.realPart;
+      imaginaryPart=imaginaryPart+right.imaginaryPart;
+      return Complex(realPart,imaginaryPart);
+} // end function add
+
+Complex Complex::subtract( const Complex &right )
+{
+   /* Write a statement to return a Complex object. Subtract
+      the realPart of right from the realPart of this Complex
+      object and subtract the imaginaryPart of right from
+      the imaginaryPart of this Complex object */
+      realPart=realPart-right.realPart;
+      imaginaryPart=imaginaryPart-right.imaginaryPart;
+      return Complex(realPart,imaginaryPart);
+} // end function subtract
+
+void Complex::printComplex()
+{
+   cout << '(' << realPart << ", " << imaginaryPart << ')';
+} // end function printComplex
+
+void Complex::setComplexNumber( double rp, double ip )
+{
+   realPart = rp;
+   imaginaryPart = ip;
+} // end function setComplexNumber
